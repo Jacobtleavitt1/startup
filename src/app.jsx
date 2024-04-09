ef5ec6e4466a890033b2b16f6535fd07a5b0fdc5
Jacob Leavitt <jacobtleavitt1@Jacobs-MBP-51.app.byu.edu>
2024-04-09 13:14:32 -0600

    Website loads without components

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div>
+      <header>
+        <h1>GoalCrush</h1>
+        <div id="logo"></div>
+        <nav>
+          <menu>
+            <li><a href="logoutabout.html" id="menu-nav-1">About</a></li>
+            <li><a href="index.html" id="menu-nav-2">Login</a></li>
+          </menu>
+        </nav>
+      </header>
+
+      <main>App components go here</main>
+
+      <footer>
+        <p id="name">Jacobtleavitt1</p>
+        <br />
+        <a href="https://github.com/Jacobtleavitt1/startup">GitHub</a>
+      </footer>
+    </div>
+  );
 }
